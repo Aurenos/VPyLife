a82621b4cdde1389798a4644d15149b0eef339ac
Aurenos <aurenos12@gmail.com>
2013-10-31 01:12:28 -0400

    Added Environment module and laid the groundwork in the Cell module

--- cell.py
+++ cell.py
@@ -1,1 +1,20 @@
+# VPyLife - Cell module
+# Kile Deal
+
 from visual import box, color, materials
+
+class Cell(box):
+	def __init__(self, length=1, height=1, width=1, pos=(0,0,0), color=color.white, material=None, opacity=1.0,
+					visible=True, alive=False):
+		box.__init__(self) # call super constructor
+		
+		self.length = length
+		self.height = height
+		self.width = width
+		self.pos = pos
+		self.color = color
+		self.material = material
+		self.opacity = opacity
+		self.visible = visible
+		self.alive = alive
+
---
+++ environment.py
@@ -0,0 +1,8 @@
+# VPyLife - Environment module
+# Kile Deal
+
+from visual import * 
+from cell import Cell
+
+class Environment:
+	pass
