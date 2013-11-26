10fe54af16343b9adb34fa2fe0b95d45e39bb792
Aurenos <aurenos12@gmail.com>
2013-11-26 02:00:02 -0500

    Modified cell age changes

--- src/environment.py
+++ src/environment.py
@@ -61,14 +61,14 @@ class Environment:
 					kill_list.append(cell)
 				else:
 					cell.age += 1
-					if cell.age == 2:
-						cell.color = age_colors[0]
-					elif cell.age == 4:
-						cell.color = age_colors[1]
-					elif cell.age == 6:
-						cell.color = age_colors[2]
-					elif cell.age == 8:
+					if cell.age >= 8:
+						cell.color = age_colors[4]
+					elif cell.age >= 6:
 						cell.color = age_colors[3]
+					elif cell.age >= 4:
+						cell.color = age_colors[2]
+					elif cell.age >= 2:
+						cell.color = age_colors[1]
 			else:
 				if cell.active_links in self.birth_conditions:
 					birth_list.append(cell)
